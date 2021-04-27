e61f99c0cf3afcb2b4bff464f0d4b09da0d06d66
Dr-Specimen <jose.j.sarmento@me.com>
2021-04-27 23:48:41 +0100

    remove unnecessary files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-size=${1-64} 
-cpu=${2-ex5_LITTLE}
-
-make clean && make compile_gem5 SVE=1 OUTPUT=1 && make run_gem5 S=$size CPU=$cpu
-#diff output_gem5.out files_output/output_gem5_${size}.out > ${size}.diff
-#cat ${size}.diff
-
